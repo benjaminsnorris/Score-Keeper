a8eab2bf058688e91562c0db8bc3067f1b7f42ec
Ben Norris <ben@bsn.io>
2014-09-11 10:15:23 -0600

    Refactored reset and refined visual design

--- Score Keeper/SKViewController.m
+++ Score Keeper/SKViewController.m
@@ -16,6 +16,7 @@ static CGFloat scoreViewHeight = 100;
 @interface SKViewController () <UITextFieldDelegate>
 
 @property (nonatomic, strong) UIScrollView *scrollView;
+@property (nonatomic, strong) UIBarButtonItem *resetButton;
 
 @end
 
@@ -30,13 +31,16 @@ static CGFloat scoreViewHeight = 100;
     
     self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
     [self.view addSubview:self.scrollView];
+    self.scrollView.backgroundColor = [UIColor grayColor];
     
     [self initializeViews];
     
-    // Deal with the nav bar buttons
+    // Add the add button
     UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addNewScore)];
     self.navigationItem.rightBarButtonItem = addButton;
-        
+    
+    // Initialize the reset button
+    self.resetButton = [[UIBarButtonItem alloc] initWithTitle:@"Reset" style:UIBarButtonItemStylePlain target:self action:@selector(resetScores)];
 }
 
 - (void)initializeViews {
@@ -47,9 +51,9 @@ static CGFloat scoreViewHeight = 100;
 
 - (void)addNewScore {
     [self addScoreView:currentNumberOfScores];
-    UIBarButtonItem *resetButton = [[UIBarButtonItem alloc] initWithTitle:@"Reset" style:UIBarButtonItemStylePlain target:self action:@selector(resetScores)];
-    self.navigationItem.leftBarButtonItem = resetButton;
-
+    
+    // Set the rest button to be the left button of the nav bar
+    self.navigationItem.leftBarButtonItem = self.resetButton;
 }
 
 - (void)resetScores {
@@ -64,11 +68,9 @@ static CGFloat scoreViewHeight = 100;
 }
 
 - (void)addScoreView:(int) theIndex {
-    UIView *scoreView = [[UIView alloc] initWithFrame:CGRectMake(0, scoreViewHeight * theIndex, self.view.frame.size.width, scoreViewHeight)];
+    UIView *scoreView = [[UIView alloc] initWithFrame:CGRectMake(0, scoreViewHeight * theIndex + currentNumberOfScores, self.view.frame.size.width, scoreViewHeight)];
     [self.scrollView addSubview:scoreView];
     scoreView.backgroundColor = [UIColor whiteColor];
-    scoreView.layer.borderColor = [UIColor blackColor].CGColor;
-    scoreView.layer.borderWidth = 1;
     
     UITextField *nameField = [[UITextField alloc] initWithFrame:CGRectMake(margin, 0, 125, scoreViewHeight)];
     nameField.placeholder = @"Name";
@@ -91,7 +93,7 @@ static CGFloat scoreViewHeight = 100;
     currentNumberOfScores++;
     
     // Increase the size of the scrollView to hold all of the views
-    self.scrollView.contentSize = CGSizeMake(self.view.frame.size.width, currentNumberOfScores * scoreViewHeight);
+    self.scrollView.contentSize = CGSizeMake(self.view.frame.size.width, currentNumberOfScores * scoreViewHeight + currentNumberOfScores);
 }
 
 - (void)updateScore:(UIStepper *) scoreStepperUpdating {
